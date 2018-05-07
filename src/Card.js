2f4fb239beb982a38b8e19a7150961ce91e8b950
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-07 17:51:23 +0200

    Display the card if feedback is not hidden, add the proper card class

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,12 @@
-const Card = () => <div className="card" />
 import React from "react";
 import "./Card.css";
 const HIDDEN_SYMBOL = "❓";
+
+const Card = ({card, feedback}) => (
+	<div className={`card ${feedback}`}>
+		<span className="symbol">
+			{feedback === "hidden" ? HIDDEN_SYMBOL : card}
+		</span>
+	</div>
+);
 export default Card;
