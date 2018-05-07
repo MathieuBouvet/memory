22805b90f34641f5d1cb0864d28fae9499c1ad05
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-07 17:07:58 +0200

    Format Card.js : double quotes strings and semicolons

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,5 @@
-import React from 'react'
-
-import './Card.css'
-
-const HIDDEN_SYMBOL = '❓'
-
 const Card = () => <div className="card" />
-
-export default Card
+import React from "react";
+import "./Card.css";
+const HIDDEN_SYMBOL = "❓";
+export default Card;
